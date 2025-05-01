f9a9e883612a9fa395b8719f9e24770d7cdf5257
SemenovDev <semenovdev@yahoo.com>
2025-05-01 14:01:21 +0600

    view binding

--- app/src/main/java/com/semenovdev/services/MainActivity.kt
+++ app/src/main/java/com/semenovdev/services/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.semenovdev.services
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.semenovdev.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
